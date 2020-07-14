1d084234a23146b1e98033463f49f648b55ae72e
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-14 12:59:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,18 +17,13 @@ function nowServing(katzDeliLine) {
 function currentLine(array) {
   var newarray = ""
   for (let i = 0; i < array.length; i++) {
-      if (i + 1 === array.length) {
-        newarray = newarray + (i + 1) + `. ${array[i]}`
-      }
-      else {
-        newarray = newarray + (i + 1) + `. ${array[i]}, `
-      }
-      //could also use if and else statement inside of for loop instead of slice
+      newarray = newarray + (i + 1) + `. ${array[i]}, `
+      //could also use if and else statement in for loop instead of slice
   }
   if (array.length === 0) {
     return "The line is currently empty."
   }
   else if (array.length > 0) {
-    return `The line is currently: ${newarray}`
+    return `The line is currently: ${newarray.slice(0, -2)}`
   }
 }
