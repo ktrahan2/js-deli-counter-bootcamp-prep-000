0e0288aef47fe9c95a25e63b2b21e07f54e06adb
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-14 12:51:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-function takeANumber(currentline, name) {
-  currentline.push(name)
-  return `Welcome, ${name}. You are number ${currentline.length} in line.`
+function takeANumber(numpeople, name) {
+  numpeople.push(name)
+  return `Welcome, ${name}. You are number ${numpeople.length} in line.`
 }
 
 function nowServing(katzDeliLine) {
-  if (katzDeliLine.length > 0) {
-  var move = katzDeliLine.shift()
-  return `Currently serving ${move}.`
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
   }
-  else if (katzDeliLine.length === 0) {
-  return "There is nobody waiting to be served!"
+  else if (katzDeliLine.length > 0) {
+    // could just use else statement
+    var move = katzDeliLine.shift()
+    return `Currently serving ${move}.`
   }
 }
 
 function currentLine(array) {
   var newarray = ""
   for (let i = 0; i < array.length; i++) {
-    newarray = newarray + (i + 1) + `. ${array[i]}, `
+      newarray = newarray + (i + 1) + `. ${array[i]}, `
+      //could also use if and else statement to determine i + 1 === array.length, or everything else
   }
-  if (array.length > 0) {
-    return `The line is currently: ${newarray.slice(0, -2)}`
-  }
-  else if (array.length === 0) {
+  if (array.length === 0) {
     return "The line is currently empty."
+  }
+  else if (array.length > 0) {
+    return `The line is currently: ${newarray.slice(0, -2)}`
   }
 }
